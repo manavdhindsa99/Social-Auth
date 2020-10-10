2915cbbb02a0b47ec65eb06f9576d9f171cdbf6f
manavdhindsa99 <manavdhindsa13@gmail.com>
2020-10-10 13:47:48 +0530

    Updates

---
+++ precache-manifest.544b1aa2b1d8c4ef061a3b94bcbde8ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080de0b135a30e085aff96948ee32694",
+    "url": "/Social-Auth/index.html"
+  },
+  {
+    "revision": "dc91d82a7cc0a110022e",
+    "url": "/Social-Auth/static/css/main.ed8163a7.chunk.css"
+  },
+  {
+    "revision": "4d4e7e8eb7117a260eb2",
+    "url": "/Social-Auth/static/js/2.de996d38.chunk.js"
+  },
+  {
+    "revision": "3a35fc6979a146f72e27294220a007c5",
+    "url": "/Social-Auth/static/js/2.de996d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc91d82a7cc0a110022e",
+    "url": "/Social-Auth/static/js/main.2d31789a.chunk.js"
+  },
+  {
+    "revision": "ce8813c5740afe6926e7",
+    "url": "/Social-Auth/static/js/runtime-main.5c7fbc7d.js"
+  }
+]);
